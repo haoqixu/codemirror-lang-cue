3d094119705dd6c30da420fa92aa2ae04a7a0c83
haoqixu <hq.xu0o0@gmail.com>
2024-12-26 16:03:58 +0800

    replace language name

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {parser} from "./syntax.grammar"
 import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
 import {styleTags, tags as t} from "@lezer/highlight"
 
-export const EXAMPLELanguage = LRLanguage.define({
+export const cueLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
@@ -25,6 +25,6 @@ export const EXAMPLELanguage = LRLanguage.define({
   }
 })
 
-export function EXAMPLE() {
-  return new LanguageSupport(EXAMPLELanguage)
+export function cue() {
+  return new LanguageSupport(cueLanguage)
 }
